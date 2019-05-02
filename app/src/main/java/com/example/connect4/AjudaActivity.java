0281c82a9c16c9b646e451b5556a26dd59f8bf6c
Pablo <serranopablonavarro@gmail.com>
2019-05-02 19:14:13 +0200

    Ajuda layout i funcionalitat

---
+++ app/src/main/java/com/example/connect4/AjudaActivity.java
@@ -0,0 +1,24 @@
+package com.example.connect4;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.Button;
+
+public class AjudaActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.Ajuda);
+        Button tornar = findViewById(R.id.Tornar);
+        tornar.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(AjudaActivity.this, MainActivity.class);
+            }
+        });
+
+    }
+}
