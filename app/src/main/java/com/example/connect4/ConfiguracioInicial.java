022f85f4d6223d38e2e9fb0876034dbdf1fd925a
Tuset13 <37141132+Tuset13@users.noreply.github.com>
2019-05-10 13:37:57 +0200

    Configuration Bundle Done

--- app/src/main/java/com/example/connect4/ConfiguracioInicial.java
+++ app/src/main/java/com/example/connect4/ConfiguracioInicial.java
@@ -5,6 +5,10 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.CheckBox;
+import android.widget.EditText;
+import android.widget.RadioButton;
+import android.widget.RadioGroup;
 
 public class ConfiguracioInicial extends Activity implements View.OnClickListener{
 
@@ -23,7 +27,17 @@ public class ConfiguracioInicial extends Activity implements View.OnClickListene
 
         switch (v.getId()) {
             case R.id.start_button:
+                Bundle data = new Bundle();
+                EditText editText = findViewById(R.id.editTextAlias);
+                RadioGroup graella = findViewById(R.id.Graella1);
+                int selectedId = graella.getCheckedRadioButtonId();
+                RadioButton radioButton = findViewById(selectedId);
+                CheckBox temps = findViewById(R.id.checkBox);
+                data.putString(getString(R.string.aliaskey), editText.getText().toString());
+                data.putInt(getString(R.string.graellakey), Integer.parseInt(radioButton.getText().toString()));
+                data.putBoolean(getString(R.string.tempskey), temps.isChecked());
                 Intent intent = new Intent(this, GameActivity.class);
+                intent.putExtras(data);
                 startActivity(intent);
                 break;
         }
