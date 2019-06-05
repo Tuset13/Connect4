c91e8c8ae2d93351979f3e8e56a9a635779c10c4
psn2 <serranopablonavarro@gmail.com>
2019-06-05 12:56:33 +0200

    sa

--- app/src/main/java/com/example/connect4/GameActivity.java
+++ app/src/main/java/com/example/connect4/GameActivity.java
@@ -1,7 +1,6 @@
 package com.example.connect4;
 
 import android.app.Activity;
-import android.content.Context;
 import android.content.Intent;
 import android.content.SharedPreferences;
 import android.os.Bundle;
