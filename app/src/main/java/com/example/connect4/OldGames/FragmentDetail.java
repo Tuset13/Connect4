f5770b5ac28cece7683816c0a185a5cd3479c107
Pablo <serranopablonavarro@gmail.com>
2019-06-09 20:22:51 +0200

    Gir de pantalla en la detailFragment

--- app/src/main/java/com/example/connect4/OldGames/FragmentDetail.java
+++ app/src/main/java/com/example/connect4/OldGames/FragmentDetail.java
@@ -14,6 +14,9 @@ import com.example.connect4.R;
 
 public class FragmentDetail extends Fragment {
 
+    String gameInfo;
+    View view;
+
     @Override
     public void onCreate(Bundle savedInstanceState){
         super.onCreate(savedInstanceState);
@@ -25,14 +28,25 @@ public class FragmentDetail extends Fragment {
     }
 
     @Override
-    public View onCreateView(LayoutInflater inflater, ViewGroup container,
-                             Bundle savedInstanceState){
-        View view = inflater.inflate(R.layout.fragment_detail_old_games,container,false);
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState){
+        view = inflater.inflate(R.layout.fragment_detail_old_games,container,false);
+
+        build(savedInstanceState);
+
         return view;
     }
 
+    private void build(Bundle savedInstanceState) {
+        TextView txt = view.findViewById(R.id.gameInfo);
+
+        if(savedInstanceState != null && (savedInstanceState.getSerializable("txt") != null)) {
+            gameInfo = (String) savedInstanceState.getSerializable("txt");
+            txt.setText(gameInfo);
+        }
+    }
+
     public void showGame(String id){
-        TextView view = getView().findViewById(R.id.gameInfo);
+        TextView txt = getView().findViewById(R.id.gameInfo);
         String[] gameId = new String[]{id};
 
         PartidasSQLiteHelper usdbh = new PartidasSQLiteHelper(
@@ -43,9 +57,15 @@ public class FragmentDetail extends Fragment {
         Cursor c = db.query(
                 "Partidas", campos, "_id=?", gameId, null,null,null);
         c.moveToFirst();
-        String gameInfo = c.getString(1) + "\n" + c.getString(2) + "\n" + c.getString(3) + "\n" +
+        gameInfo = c.getString(1) + "\n" + c.getString(2) + "\n" + c.getString(3) + "\n" +
                 c.getString(4) + "\n" + c.getString(5) + "\n" + c.getString(6);
 
-        view.setText(gameInfo);
+        txt.setText(gameInfo);
+    }
+
+    @Override
+    public void onSaveInstanceState(Bundle state) {
+        super.onSaveInstanceState(state);
+        state.putSerializable("txt",  gameInfo);
     }
 }
